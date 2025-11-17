6d5354b12abd65176be075744c4c38c31f7bd4eb
Ivan <ivanbogachenco@gmail.com>
2025-11-17 19:05:06 +0300

    Add SettingsProvider to main app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'scan_controller.dart';
 import 'scan_screen.dart';
+import 'providers/settings_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Создаем провайдер настроек и загружаем настройки
+  final settingsProvider = SettingsProvider();
+  await settingsProvider.loadSettings();
+  
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => ScanController())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => ScanController()),
+        ChangeNotifierProvider.value(value: settingsProvider),
+      ],
       child: const MyApp(),
     ),
   );
